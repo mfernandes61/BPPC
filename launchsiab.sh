8637b5e020d4ee59e8eadb37e5b431d9811aabd7
Mark Fernandes <mfernandes61@gmail.com>
2016-12-19 12:19:28 +0000

    Update launchsiab.sh

--- launchsiab.sh
+++ launchsiab.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Launching SIAB!"
-/usr/local/bin/shellinaboxd -t -b && tail -f /dev/null
+/usr/local/bin/shellinaboxd --css white-on-black.css -t -b && tail -f /dev/null
 echo "Running SIAB"
